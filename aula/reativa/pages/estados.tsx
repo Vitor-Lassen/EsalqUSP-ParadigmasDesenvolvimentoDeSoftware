262a39e45aadf84988757eeab9536d1633c3c292
Vitor Lassen <vitor@lassen.com.br>
2025-01-07 19:57:14 -0300

    reativo

--- aula/reativa/pages/estados.tsx
+++ aula/reativa/pages/estados.tsx
@@ -1,7 +1,25 @@
 import styles from "../styles/Home.module.css"; // Importa os estilos CSS
+import { useState , useEffect} from "react"; // Importa o hook useState
 
 function ContadorComMensagem() {
-  return <main className={styles.main}></main>;
+  const [contador, setContador] = useState(0); // Inicializa o estado contador com o valor 0
+
+  useEffect(() => {
+    console.log(`O contador mudou para: ${contador}`);
+  },[contador]);
+
+
+  const incrementar = () => {
+    setContador(contador + 1); // Função que incrementa o contador
+    console.log(contador);
+  };
+  
+  return (
+  <main className={styles.main}>
+    <p className={styles.aula}>Você clicou {contador}</p>
+    <button onClick={incrementar} className={styles.button}>
+      Clique aqui</button>
+  </main>);
 }
 
 export default ContadorComMensagem;
